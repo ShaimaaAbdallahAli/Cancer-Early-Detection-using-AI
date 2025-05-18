ec306804002cd1f4909097cc24247d6ba5228c96
Shaimaa Khattab <137608852+ShaimaaAbdallahAli@users.noreply.github.com>
2025-05-18 23:04:56 +0300

    Add files via upload

---
+++ DOCTORS-ACCOUNT/UPCOMING-SCHEDUELS-AS-DOC/js/script.js
@@ -0,0 +1,11 @@
+function gotoscheduled(){
+    var url ="../SCHEDULED-PAITENTS/index.html"
+    window.location= url
+}
+function gotodetails(){
+    var url = "../PAITENT-DETAILS/PatientsDetails.html"
+    window.location = url 
+}
+function paitents() {
+    window.location.href = "../ALL-PAITENTS/index.html"
+}
